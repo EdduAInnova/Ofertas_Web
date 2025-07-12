48752008fc119c8c43799523f1799ea08015dd6b
EdduAInnova <edduainnova@gmail.com>
2025-07-12 14:34:00 -0500

    feat: Añadir configuración de Vercel para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Esta sección es clave para el desarrollo con Vercel CLI.
+  // Le dice a Vite que esté disponible en la red local,
+  // permitiendo que el proxy de 'vercel dev' se conecte a él sin problemas.
+  server: {
+    host: true
+  }
 })
